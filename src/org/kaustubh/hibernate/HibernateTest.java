cd6b238fa4d1963a6516dfce4af6befe87e938a4
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-01-11 17:20:30 +0530

    More annotations and fetching saved object details added

--- src/org/kaustubh/hibernate/HibernateTest.java
+++ src/org/kaustubh/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.kaustubh.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
@@ -13,13 +15,25 @@ public class HibernateTest {
 	
 	public static void main(String[] args) {
 		UserDetails user = new UserDetails();
-		user.setUserId(3);
-		user.setUserName("Third User");
+		user.setUserId(1);
+		user.setUserName("First User");
+		user.setAddress("First User Address");
+		user.setJoinedDate(new Date());
+		user.setDescription("First user description goes here");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit();
+		session.close();
+		
+		user = null;
+		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user = (UserDetails)session.get(UserDetails.class, 1);
+		System.out.println("User Name Retrieved is " + user.getUserName());
+		
 	}
 }
