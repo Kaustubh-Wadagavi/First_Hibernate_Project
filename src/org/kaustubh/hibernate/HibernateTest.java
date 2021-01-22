667f426ee61364588ac5fd36f625fa5b86c1283a
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-01-22 17:50:05 +0530

    Auto generating primary key added

--- src/org/kaustubh/hibernate/HibernateTest.java
+++ src/org/kaustubh/hibernate/HibernateTest.java
@@ -16,25 +16,29 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		
 		UserDetails user = new UserDetails();
-		user.setUserId(1);
 		user.setUserName("First User");
-		user.setAddress("First User Address");
-		user.setJoinedDate(new Date());
-		user.setDescription("First user description goes here");
+		
+		UserDetails user2 = new UserDetails();
+		user2.setUserName("Second User");
+		//user.setAddress("First User Address");
+		//user.setJoinedDate(new Date());
+		//user.setDescription("First user description goes here");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.save(user);
+		session.save(user2);
 		session.getTransaction().commit();
 		session.close();
 		
-		user = null;
+		/*user = null;
 		
 		session = sessionFactory.openSession();
 		session.beginTransaction();
 		user = (UserDetails)session.get(UserDetails.class, 1);
 		System.out.println("User Name Retrieved is " + user.getUserName());
+		*/
 		
 	}
 }
--- src/org/kaustubh/krishagni/dto/UserDetails.java
+++ src/org/kaustubh/krishagni/dto/UserDetails.java
@@ -1,30 +1,32 @@
 package org.kaustubh.krishagni.dto;
 
-import java.util.Date;
+//import java.util.Date;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
+//import javax.persistence.Basic;
+//import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
+//import javax.persistence.Lob;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.Transient;
+//import javax.persistence.Temporal;
+//import javax.persistence.TemporalType;
+//import javax.persistence.Transient;
 
 @Entity 
 @Table (name="USER_DETAILS")
 public class UserDetails {
 	//we can annotate attributes also to change the column names and getters as well.
-	@Id
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private int userId;
 	private String userName;
 	
-	@Temporal (TemporalType.DATE)
+	/*@Temporal (TemporalType.DATE)
 	private Date joinedDate;
 	private String Address;
 	@Lob
-	private String description;
+	private String description;*/
 	
 	
 	public int getUserId() {
@@ -39,7 +41,8 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public Date getJoinedDate() {
+	
+	/*public Date getJoinedDate() {
 		return joinedDate;
 	}
 	public void setJoinedDate(Date joinedDate) {
@@ -56,6 +59,6 @@ public class UserDetails {
 	}
 	public void setDescription(String description) {
 		this.description = description;
-	}
+	}*/
 	
 }
