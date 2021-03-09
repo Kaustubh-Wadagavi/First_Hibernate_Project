dc638a18787507b2e8d50acde766b09e29f6de89
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-03-09 16:33:52 +0530

    Inheritance added for single table strategy

--- src/org/kaustubh/krishagni/dto/FourWheeler.java
+++ src/org/kaustubh/krishagni/dto/FourWheeler.java
@@ -1,8 +1,10 @@
 package org.kaustubh.krishagni.dto;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Car")
 public class FourWheeler extends Vehicle {
 
 	private String SteeringWheel;
--- src/org/kaustubh/krishagni/dto/TwoWheeler.java
+++ src/org/kaustubh/krishagni/dto/TwoWheeler.java
@@ -1,8 +1,10 @@
 package org.kaustubh.krishagni.dto;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Bike")
 public class TwoWheeler extends Vehicle {
 
 	private String SteeringHandle;
