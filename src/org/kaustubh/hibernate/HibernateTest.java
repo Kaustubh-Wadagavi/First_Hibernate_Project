36577b1470f31dd7936f30f9deb8fee620bef75b
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-03-11 15:58:47 +0530

    Crud operations added

--- src/org/kaustubh/hibernate/HibernateTest.java
+++ src/org/kaustubh/hibernate/HibernateTest.java
@@ -1,15 +1,9 @@
 package org.kaustubh.hibernate;
 
-import java.util.Date;
-
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
-import org.kaustubh.krishagni.dto.Address;
-import org.kaustubh.krishagni.dto.FourWheeler;
-import org.kaustubh.krishagni.dto.TwoWheeler;
 import org.kaustubh.krishagni.dto.UserDetails;
-import org.kaustubh.krishagni.dto.Vehicle;
 
 public class HibernateTest {
 	/**
@@ -17,27 +11,15 @@ public class HibernateTest {
 	 */
 	public static void main(String[] args) {
 		
-		Vehicle vehicle = new Vehicle();
-		vehicle.setVehicleName("Car");
-		
-		TwoWheeler bike = new TwoWheeler();
-		bike.setVehicleName("Bike");
-		bike.setSteeringHandle("Bike Steering Handle");
-		
-		FourWheeler car = new FourWheeler();
-		car.setVehicleName("Porche");
-		car.setSteeringWheel("Porche Steering Wheel");
-		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
-		
 		session.beginTransaction();
-		session.save(vehicle);
-		session.save(bike);
-		session.save(car);
+		
+		UserDetails user = (UserDetails)session.get(UserDetails.class, 6);
+		user.setUserName("Updated user");
+		session.update(user);
 		
 		session.getTransaction().commit();
-		
-		session.close();	
+		session.close();
 	}
 }
--- src/org/kaustubh/krishagni/dto/UserDetails.java
+++ src/org/kaustubh/krishagni/dto/UserDetails.java
@@ -1,49 +1,19 @@
 package org.kaustubh.krishagni.dto;
 
-
-import java.util.ArrayList;
-import java.util.Collection;
-
-import javax.persistence.CascadeType;
-
-//import java.util.Date;
-
-//import javax.persistence.Basic;
-//import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-//import javax.persistence.Lob;
-import javax.persistence.Table;
-import javax.persistence.JoinColumn;
 
 @Entity 
-@Table (name="USER_DETAILS")
 public class UserDetails {
-	
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private int userId;
 	private String userName;
-	@OneToMany(cascade=CascadeType.ALL)
-	private Collection<Vehicle> vehicle = new ArrayList<Vehicle>();
-	
-	public Collection<Vehicle> getVehicle() {
-		return vehicle;
-	}
-	
-	public void setVehicle(Collection<Vehicle> vehicle) {
-		this.vehicle = vehicle;
-	}
 	
 	public int getUserId() {
 		return userId;
 	}
-	
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
